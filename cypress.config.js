eaf7dfc729e803b6a23fc71ec9b9f1cc6c7f80f4
Yury <example.com>
2024-02-14 15:58:40 +0100

    Update reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    "overwrite": false,
+    "html": false,
+    "json": true
+  },
   e2e: {
     baseUrl: 'https://example.cypress.io',
-    reporter: 'cypress-mochawesome-reporter',
+    //reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
+      //require('cypress-mochawesome-reporter/plugin')(on);
     }
   },
 });
